ed56b949127bee0f633cd3bb2880ee7910a39971
Ian Tester <imroykun@gmail.com>
2015-06-27 06:15:27 +1000

    Update Doxygen-generated HTML

--- search/all_c.js
+++ search/all_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['topic',['topic',['../classMQTT_1_1Publish.html#afc061d9d181014b6c86dc51b37bca51d',1,'MQTT::Publish']]],
-  ['type',['type',['../classMQTT_1_1Message.html#a04fc22d8c243b42f62e28f206674e8bc',1,'MQTT::Message']]]
+  ['type',['type',['../classMQTT_1_1Message.html#a8d9069807a7f8c0ea2f9fa38402e13e2',1,'MQTT::Message']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['message',['Message',['../classMQTT_1_1Message.html#a9858fc94f0acfd013e1aa913bc910a4f',1,'MQTT::Message::Message(uint8_t t, uint8_t f=0)'],['../classMQTT_1_1Message.html#ae75e91efef8bd4546124d039076a5377',1,'MQTT::Message::Message(uint8_t t, uint16_t pid)']]]
+  ['message',['Message',['../classMQTT_1_1Message.html#a481e724da9dfb0b16b140c410f1147af',1,'MQTT::Message::Message(message_type t, uint8_t f=0)'],['../classMQTT_1_1Message.html#af7b7cb548cbb70939ceea2da40393f85',1,'MQTT::Message::Message(message_type t, uint16_t pid)']]]
 ];
